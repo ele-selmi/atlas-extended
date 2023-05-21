9b338777652b94f72801dfc77a42f01bf2b755d0
ele-selmi <133996145+ele-selmi@users.noreply.github.com>
2023-05-21 16:26:27 +0100

    Update Beginning.sh

--- ATLAS_1/Beginning.sh
+++ ATLAS_1/Beginning.sh
@@ -5,27 +5,29 @@
 #                                                     #   
 #######################################################
 
-echo '1 = create all output directories'
-echo '0 = remove all output directories'
+echo 'Welcome to ATLAS 13 TeV Open Data C++ framework!!!'
+echo 'Input your option now'
+echo '1 = create all output directories (do it at the very beginning just once)'
+echo '0 = remove all output directories (in case you want to clean and begin from zero)'
 read choice
 
-# names of 2 analyses
-analysisCollection=("HWWAnalysis" "HZZAnalysis")
+# names of 12 analyses
+analysisCollection=("WBosonAnalysis" "ZBosonAnalysis" "TTbarAnalysis" "SingleTopAnalysis" "WZDiBosonAnalysis" "ZZDiBosonAnalysis" "HWWAnalysis" "HZZAnalysis" "ZTauTauAnalysis" "HyyAnalysis" "SUSYAnalysis" "ZPrimeBoostedAnalysis")
 
 # creating the output directory for the Analyses codes
 if (( $choice == 1 )) ; then
-  for i in {0..1} ; do
+  for i in {0..11} ; do
     analysisName=${analysisCollection[$i]}
     echo "Creating the directory: Analysis/${analysisName}/Output_${analysisName}"
     mkdir Analysis/${analysisName}/Output_${analysisName}
-  done
+    done
 
   echo "Creating the directory for the Plotting code: Plotting/histograms"
   mkdir Plotting/histograms
 
 # removing everything within directories
 elif  (( $choice == 0 )) ; then
-  for i in {0..1} ; do
+  for i in {0..11} ; do
     analysisName=${analysisCollection[$i]}
     echo "Removing the directory: Analysis/${analysisName}/Output_${analysisName}"
     rm -rf Analysis/${analysisName}/Output_${analysisName}
